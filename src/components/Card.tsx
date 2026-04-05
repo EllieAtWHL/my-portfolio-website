1607a9c699b1722a23091fdd7bb0fcfba61d7a01
Ellie Matthewman <30699031+EllieAtWHL@users.noreply.github.com>
2026-04-05 23:39:33 +0100

    makes the card component static when not clickable

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -37,7 +37,7 @@ export function Card({
     lg: variant === 'accent' ? '!pt-8 !pr-8 !pb-8 !pl-8' : 'p-8'
   };
   
-  const hoverClasses = hover ? 'hover:shadow-xl hover:-translate-y-1' : '';
+  const hoverClasses = hover ? 'hover:shadow-xl hover:-translate-y-1 hover-enabled' : 'no-hover';
   const interactiveClasses = onClick ? 'cursor-pointer' : '';
   
   const classes = [
